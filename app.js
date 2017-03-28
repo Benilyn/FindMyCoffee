198bb96ccf2aad2c58f8afb8e64776b1f2cd6714
Benilyn <benilyn.gonzales@gmail.com>
2017-03-28 04:35:20 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+function showUI(){
+	$('button').removeClass('hide');
+} //showUI
+
+$(document).ready(function(){
+	$('button').click(function(event){
+		$(this).addClass('hide');
+		initMap();
+	}); //click function
+}); //document.ready
+
+var map;
+var infowindow;
+
+function initMap() {
+	navigator.geolocation.getCurrentPosition(function(position) {
+    	var currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
+    	map = new google.maps.Map(document.getElementById('map'), {
+	      center: currentLocation,
+	      zoom: 13
+	    }); //map
+
+	    var service = new google.maps.places.PlacesService(map);
+	    var query = {
+	      location: currentLocation,
+	      keyword: 'coffee',
+	      radius: '2000'
+	    };   //var query
+
+	    service.nearbySearch(query, searchResults);
+	    var currentPosition = new google.maps.Marker({
+	      position: currentLocation,
+	      map: map,
+	      label: 'You are here',
+    	}); //currentPosition
+
+    	function searchResults(results, status) {
+    		if (status === google.maps.places.PlacesServiceStatus.OK) {
+		        $('#search-results').removeClass('hide');
+		        var coffeeShops = results.slice(0, 10);
+		        coffeeShops.forEach(createMarker);
+	    	} //if status=OK (searchResults)
+    	} //searchResults
+
+    	var infoWINDOW;
+    	function createMarker(place){
+    		var marker = new google.maps.Marker({
+		        map: map,
+		        position: place.geometry.location,
+      		});   //var marker
+
+      		google.maps.event.addListener(marker, 'click', function() {
+		        if (infoWINDOW) { infoWINDOW.close(); }
+		        var infowindow = new google.maps.InfoWindow();
+		        infoWINDOW = infowindow;
+		        infowindow.setContent(place.name);
+		        infowindow.open(map, this);
+		    }); //addListener function
+    	} //createMarker function
+
+    }); //getCurrentPosition
+} //initMap
